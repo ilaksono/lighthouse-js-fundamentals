d3ab0b3ff9927c5bf25cc40306a50ee3109f35e1
ilaksono <ian.laksono@gmail.com>
2020-08-19 02:41:32 +0000

    hello world function more

--- hello.js
+++ hello.js
@@ -3,3 +3,14 @@ const sayHello = function (name) {
 }
 
 sayHello('John');
+
+
+const returnSayHello = function (name) {
+
+  return "Hello, " + name;
+}
+
+const greeting = returnSayHello('John');
+
+console.log(greeting);
+
