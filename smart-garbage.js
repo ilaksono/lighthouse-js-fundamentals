932e9fb5a2e4e0ad03ffe25da5be511a67342d5f
ilaksono <ian.laksono@gmail.com>
2020-08-20 19:39:51 +0000

    trash object function

---
+++ smart-garbage.js
@@ -0,0 +1,28 @@
+function smartGarbage(trash, bins) {
+  switch(trash){
+  case 'waste':
+    bins.waste++;
+    break;
+  case 'recycling':
+    bins.recycling++;
+    break;
+  case 'compost':
+    bins.compost++;
+    break;
+  default:
+    break;       
+  }
+  return bins;
+
+
+}
+
+
+
+// {
+//   waste: 4,
+//   recycling: 3,
+//   compost: 5
+// }
+console.log(smartGarbage('recycling',
+ { waste: 4, recycling: 2, compost: 5 }));
